02313b30741e7f0ca7b06867e4cb95d218b4b47b
hoangminh2901 <minh.hoang.220027@student.fulbright.edu.vn>
2024-11-29 20:31:32 +0700

    fix read trailing new line

--- read.c
+++ read.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "headers/read.h"
+#include <unistd.h>
 
-char *read_line(void) {
+char *read_line(void)
+{
     char *line = NULL;
     size_t bufsize = 0;
-    getline(&line, &bufsize, stdin);
+    ssize_t chars = getline(&line, &bufsize, stdin);
+    if (bufsize > 0 && line[chars - 1] == '\n')
+    {
+        line[chars - 1] = '\0';
+        --chars;
+    }
     return line;
+}
+
+int main(int argc, char *argv[])
+{
+    char *line;
+    while (1)
+    {
+        printf("$ ");
+        line = read_line();
+        printf("%s\n", line);
+        free(line);
+    }
+    return 0;
 }
