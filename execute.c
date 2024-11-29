90629a037ed30fbfb37e1db73aeda4cbe5ac9419
hoangminh2901 <minh.hoang.220027@student.fulbright.edu.vn>
2024-11-29 20:31:17 +0700

    add execute

--- execute.c
+++ execute.c
@@ -1,13 +1,39 @@
 #include "headers/builtin.h"
 #include "headers/non-builtin.h"
 #include "headers/execute.h"
-
-// TODO: Combine builtin and non-builtin functions
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 // Check if the command is a builtin function
 // If it is, execute it
 // If it is not, execute it as a non-builtin function
+int execute(char **args)
+{
+    if (args[0] == NULL)
+    {
+        return 1;
+    }
 
-int execute(char **args) {
-    return 0;
+    char *builtin_str[] = {
+        "help",
+        "cd",
+        "ls",
+        "exit"};
+
+    int (*builtin_func[])(char **) = {
+        &builtin_help,
+        &builtin_cd,
+        &builtin_ls,
+        &builtin_exit};
+
+    int num_builtins = sizeof(builtin_str) / sizeof(char *);
+    for (int i = 0; i < num_builtins; i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+    return launch(args);
 }
